7eeaa488487535517cecd6b40c7d99a17f9215cc
Dhruv Kanani <kananid@sheridancollege.ca>
2021-04-19 09:58:37 -0400

    added code for Customer  class

--- src/main/java/sheridan/kananid/customerdata/data/Customer.java
+++ src/main/java/sheridan/kananid/customerdata/data/Customer.java
@@ -1,4 +1,101 @@
 package sheridan.kananid.customerdata.data;
 
-public class Customer {
+import javax.persistence.*;
+import java.io.Serializable;
+@Entity
+@Table(name = "customer")
+public class Customer implements Serializable {
+
+    @Column(name = "customer_id")
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name="first_name")
+    private String firstName = "";
+
+    @Column(name="last_name")
+    private String LastName = "";
+
+    @Column(name="email")
+    private String email = "";
+
+    @Column(name="street")
+    private String street = "";
+
+    @Column(name="city")
+    private String city = "";
+
+    @Column(name="state")
+    private String state = "";
+
+    @Column(name="zip_code")
+    private String zipCode = "";
+
+    public Customer() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return LastName;
+    }
+
+    public void setLastName(String lastName) {
+        LastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
 }
--- src/main/java/sheridan/kananid/customerdata/data/CustomerDataRepository.java
+++ src/main/java/sheridan/kananid/customerdata/data/CustomerDataRepository.java
@@ -1,4 +1,9 @@
 package sheridan.kananid.customerdata.data;
 
-public class CustomerDataRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+public interface CustomerDataRepository extends JpaRepository<Customer,Integer> {
+
+
 }
